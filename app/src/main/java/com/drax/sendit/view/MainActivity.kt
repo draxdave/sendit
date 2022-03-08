3b3dcd170f5816bd93fa6a612844e073c34c2fae
Siamak <siamak.mahmoudi@lalamove.com>
2022-03-09 01:30:25 +0800

    feat: add Google single tap sign in + new icon

--- app/src/main/java/com/drax/sendit/view/MainActivity.kt
+++ app/src/main/java/com/drax/sendit/view/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.drax.sendit.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.drax.sendit.R
+import com.drax.sendit.view.profile.ProfileFragment.Companion.REQ_ONE_TAP
 import com.drax.sendit.view.qr.QrVM
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,4 +29,39 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        println("*****************")
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = Identity.getSignInClient(this).getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with your backend.
+                            Log.d("TAG", "Got ID token.")
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d("TAG", "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d("TAG", "No ID token or password!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
+                }
+            }
+        }
+    }
+    // ...
 }
