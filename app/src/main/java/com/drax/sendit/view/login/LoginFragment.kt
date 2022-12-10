e1a142e1689dd0c6b76654464aa63c4fbda619e7
Siamak <ssiasoft@gmail.com>
2022-12-11 02:40:49 +0800

    feat: release v1.0

--- app/src/main/java/com/drax/sendit/view/login/LoginFragment.kt
+++ app/src/main/java/com/drax/sendit/view/login/LoginFragment.kt
@@ -1,39 +1,19 @@
 package com.drax.sendit.view.login
 
-import android.app.Activity
-import android.content.IntentSender
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import app.siamak.sendit.BuildConfig
 import app.siamak.sendit.R
 import app.siamak.sendit.databinding.LoginFragmentBinding
 import com.drax.sendit.data.model.ModalMessage
 import com.drax.sendit.data.service.Event
-import com.drax.sendit.data.service.SenditFirebaseService
 import com.drax.sendit.domain.network.model.SignInRequest
 import com.drax.sendit.domain.network.model.SignInResponse
-import com.drax.sendit.domain.network.model.type.UserSex
 import com.drax.sendit.view.base.BaseFragment
 import com.drax.sendit.view.util.DeviceInfoHelper
 import com.drax.sendit.view.util.collect
 import com.drax.sendit.view.util.isActive
 import com.drax.sendit.view.util.modal
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import java.time.Instant
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -44,7 +24,7 @@ class LoginFragment : BaseFragment<LoginFragmentBinding, LoginVM>(LoginFragmentB
     private val deviceInfoHelper: DeviceInfoHelper by inject()
 
     private val ssoHandler: SsoHandler = SsoHandler(this, analytics, deviceInfoHelper) { event ->
-        if (isActive()) return@SsoHandler
+        if (!isActive()) return@SsoHandler
 
         when(event){
             is SsoEvent.SignInFailed -> viewModel.googleSignInFailed(event.stringId)
