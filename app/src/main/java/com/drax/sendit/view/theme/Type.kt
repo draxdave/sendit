9624e0a68756390e14df8d34d2befa0ab8ce1c07
Siamak <ssiasoft@gmail.com>
2023-03-16 16:32:05 +0800

    ddd

--- app/src/main/java/com/drax/sendit/view/theme/Type.kt
+++ app/src/main/java/com/drax/sendit/view/theme/Type.kt
@@ -2,15 +2,19 @@ package com.drax.sendit.view.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontMain,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    h1 = TextStyle(
+        fontFamily = fontDosisBoldFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 3.sp
+    ),
 )
